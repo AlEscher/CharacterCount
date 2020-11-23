118d42da4d8bb8d981d542d3ae41d7bdea1c3ddd
Maximilian P <maximilian.pursche@web.de>
2020-11-23 16:29:52 +0100

    ASM stub finnished no tests tho

--- asm_charcount.asm
+++ asm_charcount.asm
@@ -1,6 +1,13 @@
-_DATA SEGMENT
-	word_array b words 256
-_DATA ENDS
-_TEXT SEGMENT
-_TEXT ENDS
-END
+.intel_syntax noprefix
+.global asm_charcount
+
+.DATA             ;Create a near data segment.  Local variables are declared after
+	.words	DD 256 DUP(0)
+.CODE             ;Indicates the start of a code segment.
+
+loopstart:	mov dl, byte ptr [edi]          // dl=data[0] 
+			dword ptr [.words+edx*4]    // quantity[char]++
+			inc eax
+			cmp  0, [eax]
+			jne loopstart
+			mov eax , words
